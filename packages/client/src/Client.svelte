fbfd345ce766f1845b964157eb6305957ba6c1df
Nico Rehwaldt <git_nikku@nixis.de>
2019-08-16 11:58:01 +0200

    feat(client): only show restart if details fetched

--- packages/client/src/Client.svelte
+++ packages/client/src/Client.svelte
@@ -275,10 +275,12 @@
     </div>
   {/if}
 
-  <div class="footer">
+  {#if instanceDetails || runError}
+    <div class="footer">
 
-    <button class:running={ instanceDetails && instanceDetails.state === 'running' } class:completed={instanceDetails && instanceDetails.state !== 'running' } class="start-new-instance" on:click={ handleRestart }><svg height="1.2em" viewBox="0 0 12 16" version="1.1" width="1.2em" class="icon" aria-hidden="true" fill="currentColor"><path fill-rule="evenodd" d="M10.24 7.4a4.15 4.15 0 0 1-1.2 3.6 4.346 4.346 0 0 1-5.41.54L4.8 10.4.5 9.8l.6 4.2 1.31-1.26c2.36 1.74 5.7 1.57 7.84-.54a5.876 5.876 0 0 0 1.74-4.46l-1.75-.34zM2.96 5a4.346 4.346 0 0 1 5.41-.54L7.2 5.6l4.3.6-.6-4.2-1.31 1.26c-2.36-1.74-5.7-1.57-7.85.54C.5 5.03-.06 6.65.01 8.26l1.75.35A4.17 4.17 0 0 1 2.96 5z"></path></svg> Restart</button>
+      <button class:running={ instanceDetails && instanceDetails.state === 'running' } class:completed={instanceDetails && instanceDetails.state !== 'running' } class="start-new-instance" on:click={ handleRestart }><svg height="1.2em" viewBox="0 0 12 16" version="1.1" width="1.2em" class="icon" aria-hidden="true" fill="currentColor"><path fill-rule="evenodd" d="M10.24 7.4a4.15 4.15 0 0 1-1.2 3.6 4.346 4.346 0 0 1-5.41.54L4.8 10.4.5 9.8l.6 4.2 1.31-1.26c2.36 1.74 5.7 1.57 7.84-.54a5.876 5.876 0 0 0 1.74-4.46l-1.75-.34zM2.96 5a4.346 4.346 0 0 1 5.41-.54L7.2 5.6l4.3.6-.6-4.2-1.31 1.26c-2.36-1.74-5.7-1.57-7.85.54C.5 5.03-.06 6.65.01 8.26l1.75.35A4.17 4.17 0 0 1 2.96 5z"></path></svg> Restart</button>
 
-  </div>
+    </div>
+  {/if}
 
 </div>
